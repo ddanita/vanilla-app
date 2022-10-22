8063d2d4f58bd54cc3ddc8842a7bd4bf764c14cf
Danita <daniecodes@gmail.com>
2022-10-22 19:50:43 +0100

    showing current temperature and descriptions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("hello world");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let feelsElement = document.querySelector("#feel");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  feelsElement.innerHTML = Math.round(response.data.temperature.feels_like);
+}
+
+let apiKey = "20ee06e4a2eafa24db040oa751434bt6";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={lagos}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
