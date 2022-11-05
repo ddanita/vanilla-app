cf0aa62032fbe22205cec9cc00ea3dd183e103a5
Danita <daniecodes@gmail.com>
2022-11-05 01:00:18 +0100

    Displayed forecast data using the API

--- src/index.js
+++ src/index.js
@@ -29,6 +29,56 @@ function formatDate(timestamp) {
   return `${day} ${month} ${time} ~ ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let dForecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  dForecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+                <div class="col-sm-2">
+                  <div class="card">
+                    <div class="card-body">
+                  <div class="forecast-date">
+                    <p class="card-text">${formatDay(forecastDay.time)}</p>  
+                  </div>
+                  <div class="fore-icon"><img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                    forecastDay.condition.icon
+                  }.png" alt="" width="40"></div>  
+                   <div class="forecast-temperature">
+                   <span class="forecast-max">${Math.round(
+                     forecastDay.temperature.maximum
+                   )}°</span> <span class="forecast-min">${Math.round(
+          forecastDay.temperature.minimum
+        )}°</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "20ee06e4a2eafa24db040oa751434bt6";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -53,6 +103,8 @@ function displayTemperature(response) {
     `https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
+
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
